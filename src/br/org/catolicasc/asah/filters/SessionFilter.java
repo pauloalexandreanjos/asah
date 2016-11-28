959dab61159645e83882d29fcc6d3af682944dc1
pauloalexandreanjos <paulo.alexandre.anjos@gmail.com>
2016-11-28 14:16:02 -0200

    Adicionado Filtro de Session

---
+++ src/br/org/catolicasc/asah/filters/SessionFilter.java
@@ -0,0 +1,62 @@
+package br.org.catolicasc.asah.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/sistema/*")
+public class SessionFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public SessionFilter() {
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+		HttpSession session = httpRequest.getSession();
+
+		if ((session.getAttribute("id") == null) || (session.getAttribute("userName") == null)) {
+			session.invalidate();
+			httpResponse.sendRedirect("../index.html");
+		} else {
+
+			// Throwable problem = null;
+			try {
+				chain.doFilter(request, response);
+			} catch (IOException | ServletException t) {
+				// problem = t;
+				System.err.println("Erro: " + t.getMessage());
+			}
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
